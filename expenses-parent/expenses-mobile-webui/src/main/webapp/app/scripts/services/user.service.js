c618877c614e2710edf763c17755d83ed1649068
Xavier <expenses@biologeek.io>
2017-07-13 09:27:52 +0200

    commit before change

--- expenses-parent/expenses-mobile-webui/src/main/webapp/app/scripts/services/user.service.js
+++ expenses-parent/expenses-mobile-webui/src/main/webapp/app/scripts/services/user.service.js
@@ -5,10 +5,18 @@
 	'use strict';
 	var serviceModule = angular.module('myApp');
 
-	serviceModule.factory('UserService', '$cookies', [
+	serviceModule.factory('UserService', [
 			'$http',
+			'$cookies',
 			function($http, $cookies) {
 				return {
+					getUser: function(id, callbackSuccess, callbackError){
+						$http.get('/expenses/user/'+id).then(function(response) {
+							callbackSuccess(response.data);
+						},function(response){
+							callbackError(respone);
+						});
+					},
 					isAuthenticated: function() {
 						if ($cookies.get("username") && $cookies.get("token")){
 							return true;
